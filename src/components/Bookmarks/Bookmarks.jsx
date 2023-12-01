dd5ac54c02f928710e297df5818b43b6e33c2136
Ahsan Habib <habibstk97@gmail.com>
2023-12-01 18:43:44 +0600

    next work is progessing

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 
 export default function Bookmarks() {
   return (
     <div className='w-1/3'>
-      <h2 className='text-3xl'>Bookmarks</h2>
+      <h2 className='text-3xl'>Bookmarks 2</h2>
     </div>
   )
 }
